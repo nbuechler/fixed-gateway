b8aa1321668bbb38bf6fa001a35d4cfb105bbc1f
nbuechler <natebuechler@gmail.com>
2015-11-30 21:17:13 -0500

    added a better route that sends credentials

--- controllers/interceptor.js
+++ controllers/interceptor.js
@@ -12,8 +12,9 @@ exports.foo01 = function(req, res) {
        },
        method: 'GET'
    }
-  fetchUrl("http://localhost:5000/dummy", options, function(error, meta, body){
+  fetchUrl("http://localhost:5000/process-logs/" + req.query.credentials, options, function(error, meta, body){
     console.log(body.toString());
+    console.log(req.query.credentials);
     res.send(body.toString());
   });
  //  res.render('foo/f01', {
